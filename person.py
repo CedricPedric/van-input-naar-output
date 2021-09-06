b0f179e769d5738a113f1a631b15fb8c6fa2b9a9
CedricPedric <99059069@mydavinci.nl>
2021-09-06 17:44:22 +0200

    eerste commit

---
+++ person.py
@@ -0,0 +1,21 @@
+Naam = input("Type hier u naam en achternaam: ")
+Adres = input("Type hier u Adres: ")
+Postcode = input("Type hier u Postcode: ")
+Woonplaats = input("Type hier u Woonplaats: ")
+
+print("---------------------------------------------------" + "\n"
+"|  Naam      : "  + Naam + "\n"            
+"|  Adres     : " + Adres  + "\n"        
+"|  Postcode  : " + Postcode + "\n"                 
+"|  Woonplaats: " + Woonplaats +"\n"                     
+"---------------------------------------------------" + "\n"
+)
+
+print("---------------------------------------------------",
+"|  Naam      : "  + Naam,
+"|  Adres     : " + Adres,         
+"|  Postcode  : " + Postcode,               
+"|  Woonplaats: " + Woonplaats,                     
+"---------------------------------------------------",
+sep = "\n"
+)
